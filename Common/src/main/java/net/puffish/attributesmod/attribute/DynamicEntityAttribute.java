d3fd328cc2f7a32a52c3d9524792ec8d6939beb9
YoruNina <836193631@qq.com>
2024-08-03 01:48:53 +0800

    尝试修改属性默认值到0

--- Common/src/main/java/net/puffish/attributesmod/attribute/DynamicEntityAttribute.java
+++ Common/src/main/java/net/puffish/attributesmod/attribute/DynamicEntityAttribute.java
@@ -4,6 +4,6 @@ import net.minecraft.entity.attribute.EntityAttribute;
 
 public class DynamicEntityAttribute extends EntityAttribute {
 	public DynamicEntityAttribute(String translationKey) {
-		super(translationKey, Double.NaN);
+		super(translationKey, 0);
 	}
 }
